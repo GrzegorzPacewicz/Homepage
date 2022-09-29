0d5fee35332f051e426bf576753fa6255dc9bb15
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-09-29 13:22:33 +0200

    Changed readme and js function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 {
-    const buttonElement = document.querySelector(".js-button");
-    const bodyElement = document.querySelector(".js-body");
-    const themeName = document.querySelector(".js-themeName");
+    const toggleBackgroundColor = () => {
+      const bodyElement = document.querySelector(".js-body");
+      const themeName = document.querySelector(".js-themeName");
+  
+      bodyElement.classList.toggle("body--dark");
+      themeName.innerText = bodyElement.classList.contains("body--dark")
+        ? "jasny"
+        : "ciemny";
+    };
+  
+    const init = () => {
+      const buttonElement = document.querySelector(".js-button");
+  
+      buttonElement.addEventListener("click", toggleBackgroundColor);
+    };
+  
+    init();
+  }
 
-    buttonElement.addEventListener("click", () => {
-        bodyElement.classList.toggle("body--dark");
-        themeName.innerText = bodyElement.classList.contains("body--dark") ? "jasny" : "ciemny";
-    });
-}
+// Previous version
+// {
+//     const buttonElement = document.querySelector(".js-button");
+//     const bodyElement = document.querySelector(".js-body");
+//     const themeName = document.querySelector(".js-themeName");
+
+//     buttonElement.addEventListener("click", () => {
+//         bodyElement.classList.toggle("body--dark");
+//         themeName.innerText = bodyElement.classList.contains("body--dark") ? "jasny" : "ciemny";
+//     });
+// }
