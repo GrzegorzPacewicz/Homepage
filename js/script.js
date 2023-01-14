b1454cd8c3aa8c3be187109455c6482ce5752950
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-01-14 18:56:27 +0100

    Cleaned comments

--- js/script.js
+++ js/script.js
@@ -17,15 +17,3 @@
   
     init();
   }
-
-// Previous version
-// {
-//     const buttonElement = document.querySelector(".js-button");
-//     const bodyElement = document.querySelector(".js-body");
-//     const themeName = document.querySelector(".js-themeName");
-
-//     buttonElement.addEventListener("click", () => {
-//         bodyElement.classList.toggle("body--dark");
-//         themeName.innerText = bodyElement.classList.contains("body--dark") ? "jasny" : "ciemny";
-//     });
-// }
